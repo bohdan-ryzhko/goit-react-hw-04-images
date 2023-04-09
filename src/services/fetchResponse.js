285c70c11efb21a95677c81407350015f6414b0b
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-04-09 12:11:52 +0300

    add request search images, install axios

---
+++ src/services/fetchResponse.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_KEY = "33587663-9f49167c56a6d2d024abb7fb5";
+axios.defaults.baseURL = `https://pixabay.com/api/`;
+
+export const fetchResponse = async (value, page) => {
+	const response = await axios.get(`?q=${value}`, {
+		params: {
+			key: API_KEY,
+			page,
+			image_type: "photo",
+			orientation: "horizontal",
+			per_page: 12
+		}
+	});
+
+	return response.data.hits;
+}
